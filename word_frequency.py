6410490407d55ffc26bea3e3d76ccfcf414a3b94
Sabirin Mohamed <sabirin.gca@gmail.com>
2026-03-30 17:42:19 -0400

    word_frequency.py

---
+++ word_frequency.py
@@ -0,0 +1,20 @@
+def text_word(text):
+    words = text.split()
+    
+    frequency = {}
+
+    for word in words:
+
+        if word in frequency:
+            frequency[word] += 1
+        else:
+            frequency[word] = 1
+    sorted_frequency = dict(sorted(frequency.items()))
+    
+    return sorted_frequency
+
+text = input("Type your text: ")
+print("Word List:", sorted(text_word(text).keys()))
+print("\nBag of Words:")
+for word, count in text_word(text).items():
+    print(word, "-", count)
